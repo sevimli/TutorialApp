04a45e6677d00d4033a07a95341281929709288b
sevimli <hasan_sevimli@outlook.com>
2017-08-23 13:49:54 +0300

    tum dosyalar

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Photographs pg = new Photographs();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var model = new List<Photographs>();
+
+            model.Add(new Photographs { Name = "im1",
+                Url = "/Contents/images/image_01.jpg" });
+            model.Add(new Photographs { Name = "im2",
+                Url = "/Contents/images/image_02.jpg"
+            });
+            model.Add(new Photographs { Name = "im3",
+                Url = "/Contents/images/image_03.jpg"
+            });
+            model.Add(new Photographs { Name = "im4",
+                Url = "/Contents/images/image_04.jpg"
+            });
+            model.Add(new Photographs { Name = "im5",
+                Url = "/Contents/images/image_05.jpg"
+            });
+
+            return View(model);
+        }
+    }
+}
+
+  
---
+++ Web/Models/Photographs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+  
+       public class Photographs
+        {
+            public string Location { get; set; }
+            public int Scale { get; set; }
+            public string Photographer { get; set; }
+            public string Name { get; set; }
+            public string Url { get; set; }
+
+        }
+
+}
